6fcfdb9d8b86d7a8e299f2347dcbbc39aa68cd2c
vitaly.plastinin <vitaly.plastinin@megafon.ru>
2019-01-12 00:01:24 +0300

    выполнено lesson3/easy

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -5,13 +5,28 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    """
+    Округление полученного десятичного числа до указанного количества знаков
+    """
+    # выполним проверку
+    if (ndigits < 0):
+        return
 
+    # все что оставим загоняем в целую часть и при необходимости добавляем 1
+    result = number * 10 ** ndigits
+
+    # применяем правило округления
+    if result - int(result) >= 0.5:
+        result += 1
+    result = int(result)
+
+    # двигаем разряды взад
+    result *= 10 ** -ndigits
+    return result
 
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
-
 
 # Задание-2:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
@@ -20,7 +35,11 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    if (ticket_number < 100000) or (ticket_number > 999999):
+        return False
+    first3 = int(ticket_number / 10**5) % 10 + int(ticket_number / 10**4) % 10 + int(ticket_number / 10**3) % 10
+    last3 =  int(ticket_number / 10**2) % 10 + int(ticket_number / 10**1) % 10 + int(ticket_number / 10**0) % 10
+    return first3 == last3
 
 
 print(lucky_ticket(123006))
