59fb4f8a15168493181a49ff2ba5a368b447ddc3
vitaly.plastinin <vitaly.plastinin@megafon.ru>
2019-01-16 00:05:27 +0300

    сделаны все задания lesson4

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -20,6 +20,34 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+# способ с регулярками
+import re
+
+s = re.compile("[a-z]+[A-Z]+[a-z]+")
+print(s.findall(line))
+
+# второй способ. так себе
+small = list(map(chr, range(ord('a'), ord('z')+1)))
+items = []
+
+i = j = 0
+
+while i < len(line):
+    items.append("")
+    while i < len(line):
+        if line[i] in small:
+            items[j] = items[j]+line[i]
+            i += 1
+        else:
+            break
+    while i < len(line):
+        if line[i] not in small:
+            i += 1
+        else:
+            break
+    j += 1
+
+print(items)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -45,9 +73,66 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+# способ с регулярками
+import re
+
+s = re.compile("[a-z][a-z][A-Z]+[A-Z][A-Z]")
+print(list(map(lambda x: x[2:][:-2], s.findall(line_2))))
+
+# второй способ. без комментариев
+small = list(map(chr, range(ord('a'), ord('z') + 1)))
+items = []
+
+i = 0
+while i < len(line_2):
+    big_seq = small_seq = 0
+    while i < len(line_2):
+        if line_2[i] in small:
+            small_seq += 1
+            i += 1
+        else:
+            break
+    if small_seq < 2:
+        small_seq = 0
+        i += 1
+        continue
+    while i < len(line_2):
+        if line_2[i] not in small:
+            big_seq += 1
+            i += 1
+        else:
+            break
+    if big_seq <= 2:
+        big_seq = 0
+        continue
+    items.append(line_2[i - big_seq:i - 2])
+
+print(items)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+from random import randint
+import re
+f = open("numbers.txt", "w", encoding='UTF-8')
+
+s = ""
+for i in range(2500):
+       s += str(randint(0, 9))
+f.write(s)
+f.close()
+
+f = open("numbers.txt", "r", encoding='UTF-8')
+numstr = f.readline()
+f.close()
+
+max_seq = ""
+for i in range(9):
+    digits_re = re.compile(f"{i}+")
+    for digit_seq in digits_re.findall(numstr):
+        max_seq = digit_seq if len(digit_seq) > len(max_seq) else max_seq
+
+print(f"Самая длинная последовательность одинковых цифр: {max_seq}")
