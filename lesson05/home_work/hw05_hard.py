8afb750df56c7b81bfafe446589a66628a082dea
vitaly.plastinin <vitaly.plastinin@megafon.ru>
2019-01-18 22:52:45 +0300

    выполнено lesson5/norm+hard

--- lesson05/home_work/hw05_hard.py
+++ lesson05/home_work/hw05_hard.py
@@ -13,3 +13,84 @@
 # Исходной директорией считать ту, в которой был запущен скрипт.
 
 # P.S. По возможности, сделайте кросс-платформенную реализацию.
+
+
+import os
+import sys
+
+
+def print_help():
+    print("help - получение справки")
+    print("cp <file_name> - создает копию указанного файла")
+    print("rm <file_name> - удаляет указанный файл")
+    print("cd <full_path or relative_path> - меняет текущую директорию на указанную")
+    print("ls - отображение полного пути текущей директории")
+
+
+def cmd_cp(source_fn):
+    dest_fn = source_fn + ".cpy"
+    try:
+        source = open(source_fn, "r", encoding="UTF-8")
+        content = source.readlines()
+    except Exception as e:
+        print(f"Возникла ошибка при чтении из файла {source_fn}: {e}")
+    finally:
+        source.close()
+
+    try:
+        dest = open(dest_fn, "w", encoding="UTF-8")
+        dest.writelines(content)
+    except Exception as e:
+        print(f"Возникла ошибка при записи в файл {dest_fn}: {e}")
+    finally:
+        dest.close()
+
+
+def cmd_rm(filename):
+    try:
+        os.remove(filename)
+    except Exception as e:
+        print(f"Ошибка при удалении: {e}")
+    return
+
+
+def cmd_cd(path):
+    try:
+        os.chdir(path)
+        print(f"Текущая директория: {os.getcwd()}")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+    return
+
+
+def cmd_ls():
+    print(f"Текущая директория: {os.getcwd()}")
+    return
+
+
+do = {"help": print_help,
+      "cp": cmd_cp,
+      "rm": cmd_rm,
+      "cd": cmd_cd,
+      "ls": cmd_ls}
+
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = None
+    print_help()
+
+if key != "ls":
+    try:
+        param = sys.argv[2]
+    except IndexError:
+        param = None
+else:
+    param = None
+
+if key:
+    if do.get(key):
+        do[key](param) if param else do[key]()
+    else:
+        print("Задан неверный ключ")
+        print("Укажите ключ help для получения справки")
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,76 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+import os
+import sys
+
+
+def print_menu(menu):
+    for item in menu:
+        print(item)
+    print()
+    return
+
+
+def cmd_cd():
+    dirname = input("Введите имя директории: ")
+    try:
+        os.chdir(dirname)
+        print(f"Текущая директория {os.getcwd()}")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+    return False
+
+
+def cmd_list(dirname=None):
+    print(f"Листинг директории: ")
+    for item in os.listdir():
+        print(item)
+    return False
+
+
+def cmd_delete(dirname=None):
+    dirname = input("Введите имя директории для удаления: ")
+    try:
+        os.rmdir(dirname)
+        print(f"Удалено")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+    return False
+
+
+def cmd_create(dirname=None):
+    dirname = input("Введите имя новой директории: ")
+    try:
+        os.mkdir(dirname)
+        print(f"Создано")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+    return False
+
+
+def cmd_halt():
+    return True
+
+
+halt = False
+menu = {"0. Выход": cmd_halt,
+        "1. Перейти в папку": cmd_cd,
+        "2. Просмотреть содержимое текущей папки": cmd_list,
+        "3. Удалить папку": cmd_delete,
+        "4. Создать папку": cmd_create}
+
+while not halt:
+    print_menu(menu)
+    try:
+        n = int(input("Введите операцию: "))
+    except Exception as e:
+        print("Вводите правильно! Нужно число от 0 до 4")
+        print()
+        continue
+    if (n < 0) or (n >= len(menu)):
+        print("Недопустимая операция")
+        print()
+        continue
+    halt = menu[list(menu)[n]]()
+    print()
